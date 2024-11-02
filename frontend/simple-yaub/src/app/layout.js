1dad538661ecb82aa02b669e54cc61f63713eeeb
Maxim Rostov <maksim.rostov@adevinta.com>
2024-11-02 16:47:44 +0100

    WIP: fixing layout

--- frontend/simple-yaub/src/app/layout.js
+++ frontend/simple-yaub/src/app/layout.js
@@ -1,6 +1,6 @@
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html>
       <body>
         {children}
       </body>
--- frontend/simple-yaub/src/app/post/[id]/page.js
+++ frontend/simple-yaub/src/app/post/[id]/page.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import PostPage from '@/components/PostPage';
 
-export default function PostPageWrapper({ params }) {
-    const { id } = params;
+export default async function PostPageWrapper({params}) {
+    const {id} = await params;
 
-    return <PostPage postId={id} />;
+    return <PostPage postId={id}/>;
 }
