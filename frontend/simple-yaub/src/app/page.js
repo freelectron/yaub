bf86f8a4b5f95ac75a7f28f5b38e30e3d7164be7
Maxim Rostov <maksim.rostov@adevinta.com>
2024-11-02 16:28:18 +0100

    Minor refactoring

--- frontend/simple-yaub/src/app/page.js
+++ frontend/simple-yaub/src/app/page.js
@@ -4,15 +4,7 @@ import {fetchPostsMetaInfo} from "@/components/Backend";
 export default async function Home() {
   const initialPostsMetaInfo = await fetchPostsMetaInfo();
 
-  console.log(initialPostsMetaInfo);
-  // Ensure the data has the correct structure
-  const safeData = {
-    posts: Array.isArray(initialPostsMetaInfo?.posts)
-        ? initialPostsMetaInfo.posts
-        : []
-  };
-
   return (
-      <MainPage defaultPostsMetaInfo={safeData} />
+      <MainPage defaultPostsMetaInfo={initialPostsMetaInfo} />
   );
 }
