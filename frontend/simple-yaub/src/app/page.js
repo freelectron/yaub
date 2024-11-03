20003e3a7617d3ed1146120863e2d40a7a7591bd
Maxim Rostov <maksim.rostov@adevinta.com>
2024-11-03 20:23:42 +0100

    Arrange css classes based on page

--- frontend/simple-yaub/src/app/page.js
+++ frontend/simple-yaub/src/app/page.js
@@ -2,6 +2,7 @@ import MainPage from "@/components/MainPage";
 import {fetchPostsMetaInfo} from "@/components/PublicFiles";
 
 import '../styles/global.css';
+import './page.css';
 
 export default async function Home() {
   const initialPostsMetaInfo = await fetchPostsMetaInfo();
--- frontend/simple-yaub/src/app/post/[id]/page.js
+++ frontend/simple-yaub/src/app/post/[id]/page.js
@@ -3,6 +3,7 @@ import PostPage from '@/components/PostPage';
 import {fetchPostContent} from "@/components/PublicFiles";
 
 import '../../../styles/global.css';
+import './page.css';
 
 export default async function PostPageWrapper({params}) {
     const {id} = await params;
