a680f511d0969f28b591d3a95d27208a10d4e7c6
Maxim Rostov <maksim.rostov@adevinta.com>
2024-11-03 18:58:50 +0100

    Get ridd of react-bootstrap for posts; Add simple css styling for post pages to global

--- frontend/simple-yaub/src/components/PostPage.js
+++ frontend/simple-yaub/src/components/PostPage.js
@@ -1,15 +1,10 @@
 "use client";
 import React, { useState, useEffect } from 'react';
-
 import Link from 'next/link';
-
-import { Container, Button, Form } from 'react-bootstrap';
-
 import { Comment, handleTextSelection, handleAddComment, addReply } from './Comments';
-import {renderMarkdown} from "./MDRenderer";
+import { renderMarkdown } from "./MDRenderer";
 
-
-const PostPage = ( { postContentResponse } ) => {
+const PostPage = ({ postContentResponse }) => {
     const postContent = postContentResponse || '';
 
     const [selectedText, setSelectedText] = useState('');
@@ -21,6 +16,7 @@ const PostPage = ( { postContentResponse } ) => {
     const handleTextSelectionWrapper = () => handleTextSelection(setSelectedText, setShowCommentForm);
     const handleAddCommentWrapper = (e) => handleAddComment(e, comments, setComments, newComment, setNewComment, selectedText, setSelectedText, setShowCommentForm);
     const addReplyWrapper = (commentId, replyContent) => addReply(commentId, replyContent, comments, setComments);
+
     // Listen for text selection
     useEffect(() => {
         document.addEventListener('mouseup', handleTextSelectionWrapper);
@@ -30,49 +26,43 @@ const PostPage = ( { postContentResponse } ) => {
     }, []);
 
     return (
-        <>
+        <div className="post-container">
+            <div className="post-content">
+                {renderMarkdown(postContent)}
+            </div>
 
-    <Container className="mt-2" style={{backgroundColor: 'transparent'}} >
-        {renderMarkdown(postContent)}
-    </Container>
-
-    <Container className="mt-0">
-        {/* Comment Form */}
-        {showCommentForm && (
-                <Form onSubmit={handleAddCommentWrapper} className="mt-0">
+            {/* Comment Form */}
+            {showCommentForm && (
+                <form onSubmit={handleAddCommentWrapper} className="comment-form">
                     <h5>Comment on: "{selectedText}"</h5>
-                    <Form.Group>
-                        <Form.Control
-                            as="textarea"
-                            rows={3}
+                    <div className="form-group">
+                        <textarea
+                            className="form-control"
+                            rows="3"
                             placeholder="Write your comment here..."
                             value={newComment}
                             onChange={(e) => setNewComment(e.target.value)}
                         />
-                    </Form.Group>
-                    <Button variant="primary" type="submit" className="mt-2">
-                        Add Comment
-                    </Button>
-                    <Button variant="secondary" className="mt-2 ms-2" onClick={() => setShowCommentForm(false)}>
+                    </div>
+                    <button type="submit" className="btn primary-btn">Add Comment</button>
+                    <button type="button" className="btn secondary-btn" onClick={() => setShowCommentForm(false)}>
                         Cancel
-                    </Button>
-                </Form>
+                    </button>
+                </form>
             )}
 
             {/* Comments Section */}
-            <h4 className="mt-0">Comments</h4>
+            <h4>Comments</h4>
             {comments.length === 0 ? (
                 <p>No comments yet. Highlight some text to add a comment!</p>
             ) : (
                 comments.map((comment) => (
-                    <Comment key={comment.id} comment={comment} addReply={addReplyWrapper}/>
+                    <Comment key={comment.id} comment={comment} addReply={addReplyWrapper} />
                 ))
             )}
 
-            <Link href="/" className="btn btn-secondary mt-4">Back to Blog</Link>
-        </Container>
-
-        </>
+            <Link href="/" className="btn back-btn">Back to Blog</Link>
+        </div>
     );
 };
 
