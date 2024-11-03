9ea07d513db19555ff1792c4a09dfb7691e53374
Maxim Rostov <maksim.rostov@adevinta.com>
2024-11-03 18:50:00 +0100

    Add css in the right place

--- frontend/simple-yaub/src/app/post/[id]/page.js
+++ frontend/simple-yaub/src/app/post/[id]/page.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PostPage from '@/components/PostPage';
 import {fetchPostContent} from "@/components/Backend";
 
+import '../../../styles/global.css';
+
 export default async function PostPageWrapper({params}) {
     const {id} = await params;
     const postContentResponse = await fetchPostContent({"postId":id})
