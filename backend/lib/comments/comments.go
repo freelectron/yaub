01798e014615e9885696a50acda869a9a0a588a2
Maxim Rostov <doermax@outlook.com>
2025-05-05 22:11:47 +0200

    Fixed local deploy

--- backend/lib/comments/comments.go
+++ backend/lib/comments/comments.go
@@ -1,7 +1,6 @@
 package comments
 
 import (
-	"backend/alog"
 	"backend/lib/mongodb"
 	"context"
 	"encoding/json"
@@ -16,38 +15,6 @@ type Comment struct {
 	Content         string    `bson:"Content"`
 	HighlightedText string    `bson:"HighlightedText"`
 	Replies         []Comment `bson:"Replies"`
-}
-
-func PostCommentsTest(ctx context.Context, dbClient mongodb.Client, postId string) error {
-	comments := []interface{}{
-		Comment{
-			Id:              1,
-			User:            "1221",
-			Content:         "This is a comment",
-			HighlightedText: "highlighted text",
-			Replies: []Comment{
-				{
-					Id:      2,
-					User:    "1222",
-					Content: "This is a reply",
-				},
-			},
-		},
-		Comment{
-			Id:              3,
-			User:            "1221",
-			Content:         "Another comment",
-			HighlightedText: "another highlighted text",
-			Replies:         []Comment{},
-		},
-	}
-
-	err := dbClient.PostComments(ctx, postId, comments)
-	if err != nil {
-		return fmt.Errorf("error posting comments: %w", err)
-	}
-
-	return nil
 }
 
 func AddComment(ctx context.Context, dbClient mongodb.Client, postId string, commentResponseBody io.ReadCloser) error {
@@ -66,8 +33,6 @@ func AddComment(ctx context.Context, dbClient mongodb.Client, postId string, com
 }
 
 func FetchComments(ctx context.Context, dbClient mongodb.Client, postId string) ([]byte, error) {
-	alog.Info(context.Background(), "Simulate a comments fetch for post %s", postId)
-
 	results, err := dbClient.GetPostComments(ctx, postId)
 
 	jsonString, err := bsonToJSONBytes(results)
