4afb03a958c1a468ee8b2032048b1b2695e1ce93
Maxim Rostov <doermax@outlook.com>
2025-05-05 11:15:17 +0200

    Handle error when no comments api available

--- frontend/simple-yaub/src/components/CommentsBackend.js
+++ frontend/simple-yaub/src/components/CommentsBackend.js
@@ -1,8 +1,15 @@
 
 const fetchPostComments = async (postId) => {
     const backendURL = process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:3001';
-    const response = await fetch(`${backendURL}/api/get_comments?postId=${postId}`);
-    return await response.json();
+    let res = [];
+    const urlQuery = `${backendURL}/api/get_comments?postId=${postId}`;
+    try {
+        const response = await fetch(urlQuery);
+        res = await response.json();
+    } catch (error) {
+        console.log(`Error fetching comments from ${urlQuery},`, error);
+    }
+    return res;
 }
 
 const postPostComment = async (postId, comment) => {
