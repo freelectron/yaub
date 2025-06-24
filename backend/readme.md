a5a0c3f7c2f76fad4c33597db8eecd03989f1aee
freelectron <doermax@outlook.com>
2025-06-24 17:40:13 +0200

    Add python gprc server llmer

---
+++ backend/readme.md
@@ -0,0 +1,9 @@
+# Yablo Backend
+
+## GRPC
+
+### LLMer Handler
+
+```bash
+protoc --go_out=. --go-grpc_out=. ../proto/chats.proto
+```
--- backend/webserver/handlers/llmer_handler.go
+++ backend/webserver/handlers/llmer_handler.go
@@ -1,1 +1,41 @@
 package handlers
+
+import (
+	gprcLLMer "backend/grpc/gen/service/llm_chat/v1"
+	"context"
+	"encoding/json"
+	"net/http"
+)
+
+type LLMHandler struct {
+	grpcClient gprcLLMer.LLMChatServiceClient
+}
+
+func NewDefaultLLMHandler(grpcClient gprcLLMer.LLMChatServiceClient) *LLMHandler {
+	return &LLMHandler{
+		grpcClient: grpcClient,
+	}
+}
+
+func (h *LLMHandler) StartSession(ctx context.Context, w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	var req gprcLLMer.StartSessionRequest
+	if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+		return
+	}
+
+	resp, err := h.grpcClient.ProcessText(ctx, &req)
+	if err != nil {
+		http.Error(w, "Error processing text", http.StatusInternalServerError)
+		return
+	}
+
+	if err := json.NewEncoder(w).Encode(resp); err != nil {
+		http.Error(w, "Error writing response", http.StatusInternalServerError)
+	}
+}
